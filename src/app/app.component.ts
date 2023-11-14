e8b2c9afe79cf4ba79c7fdf5f79f224b0ce187ff
Jazz <116513312+Jazz7262@users.noreply.github.com>
2023-11-14 04:09:06 +0000

    added hidden section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  
   isUrdu: boolean = false;
+  showContent: boolean = false;
+
+  showHiddenContent() {
+    this.showContent = true;
+  }
 }
